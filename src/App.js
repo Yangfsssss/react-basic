d22b89f80d238c0765dbed8a61b9239b7f1dc7d9
Yangfsssss <376476191@qq.com>
2020-02-22 23:48:36 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    // state:用于改变组件内容状态的值（动态）
+    // props:用于组件通信进行传值
+    this.state = {
+      persons: [
+        { name: '米斯特吴', count: 50 },
+        { name: 'Henry', count: 5 },
+        { name: 'Hemiah', count: 15 },
+      ],
+      otherState: 'anything',
+      showPersons: false
+    }
+  }
+
+  switchNameHandler = (newName) => {
+    // console.log('Hello');
+    // this.state.persons[0].name = "米斯特"
+    this.setState({
+      persons: [
+        { name: newName, count: 50 },
+        { name: 'Henry', count: 50000 },
+        { name: 'Hemiah', count: 15 },
+      ],
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: event.target.value, count: 50 },
+        { name: 'Henry', count: 50000 },
+        { name: 'Hemiah', count: 15 },
+      ],
+    })
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons
+    this.setState({ showPersons: !doesShow })
+  }
+
+  render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px soild blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
+    let persons = null
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            changed={this.nameChangedHandler}
+            name={this.state.persons[0].name}
+            count={this.state.persons[0].count} />
+          <Person
+            myclick={this.switchNameHandler.bind(this, '米修missu')}
+            name={this.state.persons[1].name}
+            count={this.state.persons[1].count} />
+          <Person
+            name={this.state.persons[2].name}
+            count={this.state.persons[2].count} >非常感谢</Person>
+        </div>
+      )
+    }
+
+    return (
+      <div className="App" >
+        <h1>Hello World</h1>
+        {/* <button onClick={()=>this.switchNameHandler("米修")}>更改状态值</button> */}
+        {/* <button style={style} onClick={this.switchNameHandler.bind(this, 'missu')}>更改状态值</button> */}
+        <button style={style} onClick={this.togglePersonsHandler}>内容切换</button>
+        {persons}
+      </div>
+    );
+  }
 }
 
 export default App;
